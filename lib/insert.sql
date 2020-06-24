5a6e3068afd45c5633c0004abb8a70fa833f23ea
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-24 17:06:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -11,7 +11,7 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 ("A", "Cat A", 100, "1/12", "1/15")
 ;
 
-INSERT INTO users () VALUES
+INSERT INTO users (name, age) VALUES
 
 
 ;
