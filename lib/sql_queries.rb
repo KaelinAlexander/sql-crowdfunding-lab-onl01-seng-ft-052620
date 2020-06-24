c27d5ec89904780e46c44b80bfbf9a2191c67348
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-24 17:38:34 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,7 @@ end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, SUM(pledges.amount) FROM users
-  JOIN pedges on pledges.user_id = users.id
+  JOIN pledges on pledges.user_id = users.id
   GROUP BY users.name
   ORDER BY SUM(pledges.amount);"
 end
