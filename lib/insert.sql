a205718edad9fedcae676ee77b7555b236f63469
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-24 17:06:38 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,14 @@
-INSERT INTO projects () VALUES
-
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("A", "Cat A", 100, "1/12", "1/15")
+("A", "Cat A", 100, "1/12", "1/15")
+("A", "Cat A", 100, "1/12", "1/15")
+("A", "Cat A", 100, "1/12", "1/15")
+("A", "Cat A", 100, "1/12", "1/15")
+("A", "Cat A", 100, "1/12", "1/15")
+("A", "Cat A", 100, "1/12", "1/15")
+("A", "Cat A", 100, "1/12", "1/15")
+("A", "Cat A", 100, "1/12", "1/15")
+("A", "Cat A", 100, "1/12", "1/15")
 ;
 
 INSERT INTO users () VALUES
